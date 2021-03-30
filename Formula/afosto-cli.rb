a9441dca26bfa860af2ce3bc51361304d8ed28f3
goreleaserbot <dev@afosto.com>
2021-03-30 10:24:58 +0200

    Brew formula update for cli version v0.0.12

--- Formula/afosto-cli.rb
+++ Formula/afosto-cli.rb
@@ -5,21 +5,21 @@
 class AfostoCli < Formula
   desc "CLI tool for interaction with Afosto/IO"
   homepage "https://afosto.com/"
-  version "0.0.11"
+  version "0.0.12"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/afosto/cli/releases/download/v0.0.11/cli_0.0.11_Darwin_x86_64.tar.gz"
-    sha256 "6e7565d855667f2bf9afab508ad196629dbdba5b890f4040423abfac18ec8a7c"
+    url "https://github.com/afosto/cli/releases/download/v0.0.12/cli_0.0.12_Darwin_x86_64.tar.gz"
+    sha256 "8184fa447a1f5a44200198a1781bff61c5f9f8fa40219a683fd524cc0129cedf"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/afosto/cli/releases/download/v0.0.11/cli_0.0.11_Linux_x86_64.tar.gz"
-    sha256 "4a8a46bdf81d3b0c70bac584aa458518feb7f3a388979663cd2bfcd88290c87e"
+    url "https://github.com/afosto/cli/releases/download/v0.0.12/cli_0.0.12_Linux_x86_64.tar.gz"
+    sha256 "39cd0683ff8ad403aa5249e45c1a4ca8c75094652444a0904229f81c2ed0146e"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/afosto/cli/releases/download/v0.0.11/cli_0.0.11_Linux_arm64.tar.gz"
-    sha256 "8cbd02f8797c56141ce91bc02e6c6b930932e32d5958361f8b5e7ae2ee713cfa"
+    url "https://github.com/afosto/cli/releases/download/v0.0.12/cli_0.0.12_Linux_arm64.tar.gz"
+    sha256 "53473cabe0af2f93adae3d0e321d2802faa603e5e35c092f45f769d1443f7c38"
   end
 
   def install
